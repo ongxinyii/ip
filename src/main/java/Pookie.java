9e1e20c64c961f52131f7275e52fbb708afd313f
ongxinyii <e1278125@u.nus.edu>
2025-01-24 04:37:15 +0800

    Level-3

--- src/main/java/Pookie.java
+++ src/main/java/Pookie.java
@@ -9,30 +9,44 @@ public class Pookie {
         System.out.println("Hello! I'm Pookie\n⸜(｡˃ ᵕ ˂ )⸝♡\nWhat can I do for you?");
         System.out.println("____________________________________________________________");
 
-        String input;
-        ArrayList<String> tasks = new ArrayList<>();
+        ArrayList<Task> tasks = new ArrayList<>();
 
         while (true) {
-            input = sc.nextLine();
+            String in = sc.nextLine().trim();
 
-            if (input.equals("bye")) {
+            if (in.equals("bye")) {
                 System.out.println("____________________________________________________________");
                 System.out.println("Bye. ദ്ദി(˵ •̀ ᴗ - ˵ ) ✧\nHope to see you soon!");
                 System.out.println("____________________________________________________________");
                 break;
-            } else if (input.equals("list")) {
+            } else if (in.equals("list")) {
                 System.out.println("____________________________________________________________");
                 if (!tasks.isEmpty()) {
                     for (int i = 0; i < tasks.size(); i++) {
-                        System.out.println(" ~~ " + (i + 1) + ". " + tasks.get(i) + " ~~ ");
+                        System.out.println("Here are the tasks in your list:");
+                        System.out.println(" ~~ " + (i + 1) + ". " + tasks.get(i).toString() + " ~~ ");
                     }
                 } else {
                     System.out.println("No tasks added yet.");
                 }
+            } else if (in.startsWith("mark ")) {
+                int index = Integer.parseInt(in.substring(5)) - 1;
+                tasks.get(index).markDone();
+                System.out.println("______________________________________________________________");
+                System.out.println("Nice! ꉂ (≧ヮ≦) I've marked this task as done:");
+                System.out.println(" " + tasks.get(index).toString());
+                System.out.println("______________________________________________________________");
+            } else if (in.startsWith("unmark ")) {
+                int index = Integer.parseInt(in.substring(7)) - 1;
+                tasks.get(index).markNotDone();
+                System.out.println("______________________________________________________________");
+                System.out.println("OK... (¬_¬\") I've marked this task as not done yet:");
+                System.out.println(" " + tasks.get(index).toString());
+                System.out.println("______________________________________________________________");
             } else {
-                tasks.add(input);
+                tasks.add(new Task(in));
                 System.out.println("____________________________________________________________");
-                System.out.println(" added: " + input);
+                System.out.println(" added: " + in);
                 System.out.println("____________________________________________________________");
             }
         }
