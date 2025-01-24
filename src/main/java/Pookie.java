b0f76a01ee1e5080d190d3f5fdcf2fd72b1fdc97
ongxinyii <e1278125@u.nus.edu>
2025-01-24 11:53:37 +0800

    Level-4

--- src/main/java/Pookie.java
+++ src/main/java/Pookie.java
@@ -16,7 +16,7 @@ public class Pookie {
 
             if (in.equals("bye")) {
                 System.out.println("____________________________________________________________");
-                System.out.println("Bye. ദ്ദി(˵ •̀ ᴗ - ˵ ) ✧\nHope to see you soon!");
+                System.out.println("Bye. ദ്ദി(˵ •̀ ᴗ - ˵ ) ✧\nPookie hope to see you again!");
                 System.out.println("____________________________________________________________");
                 break;
             } else if (in.equals("list")) {
@@ -24,7 +24,7 @@ public class Pookie {
                 if (!tasks.isEmpty()) {
                     for (int i = 0; i < tasks.size(); i++) {
                         System.out.println("Here are the tasks in your list:");
-                        System.out.println(" ~~ " + (i + 1) + ". " + tasks.get(i).toString() + " ~~ ");
+                        System.out.println(" ˖˚⊹ ꣑ৎ\u200E " + (i + 1) + ". " + tasks.get(i).toString() + " ˖˚⊹ ꣑ৎ\u200E ");
                     }
                 } else {
                     System.out.println("No tasks added yet.");
@@ -43,6 +43,38 @@ public class Pookie {
                 System.out.println("OK... (¬_¬\") I've marked this task as not done yet:");
                 System.out.println(" " + tasks.get(index).toString());
                 System.out.println("______________________________________________________________");
+            } else if (in.startsWith("todo ")) {
+                String taskDescription = in.substring(5).trim();
+                ToDo todoTask = new ToDo(taskDescription);
+                tasks.add(todoTask);
+                System.out.println("____________________________________________________________");
+                System.out.println("Your wish is my command! I've added this task:");
+                System.out.println(" " + todoTask);
+                System.out.println("Now you have " + tasks.size() + " tasks in the list.");
+                System.out.println("____________________________________________________________");
+            } else if (in.startsWith("deadline ")) {
+                String[] parts = in.substring(9).split(" /by ");
+                String deadlineDescription = parts[0].trim();
+                String byDate = parts[1].trim();
+                Deadline deadlineTask = new Deadline(deadlineDescription, byDate);
+                tasks.add(deadlineTask);
+                System.out.println("______________________________________________________________");
+                System.out.println("Gotcha gorgeous! I've added this task:");
+                System.out.println(" " + deadlineTask);
+                System.out.println("Now you have " + tasks.size() + " tasks in the list");
+                System.out.println("______________________________________________________________");
+            } else if (in.startsWith("event ")) {
+                String[] parts = in.substring(6).split(" /from | /to ");
+                String eventDescription = parts[0].trim();
+                String fromDate = parts[1].trim();
+                String toDate = parts[2].trim();
+                Event eventTask = new Event(eventDescription, fromDate, toDate);
+                tasks.add(eventTask);
+                System.out.println("____________________________________________________________");
+                System.out.println("Alrighty! I've added this task:");
+                System.out.println(" " + eventTask);
+                System.out.println("Now you have " + tasks.size() + " tasks in the list.");
+                System.out.println("____________________________________________________________");
             } else {
                 tasks.add(new Task(in));
                 System.out.println("____________________________________________________________");
