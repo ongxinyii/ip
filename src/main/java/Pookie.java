f43432f2d8d0a2943571f02656a1a748d30c3fc6
ongxinyii <e1278125@u.nus.edu>
2025-01-24 13:28:33 +0800

    Level-6

--- src/main/java/Pookie.java
+++ src/main/java/Pookie.java
@@ -2,6 +2,11 @@ import java.util.Scanner;
 import java.util.ArrayList;
 
 public class Pookie {
+    public enum TaskType {
+        TODO,
+        DEADLINE,
+        EVENT
+    }
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
@@ -15,16 +20,16 @@ public class Pookie {
             String in = sc.nextLine().trim();
 
             try {
-                if (in.equals("bye")) {
+                if (in.equalsIgnoreCase("bye")) {
                     System.out.println("____________________________________________________________");
-                    System.out.println("Bye Princess! ദ്ദി(˵ •̀ ᴗ - ˵ ) ✧\nPookie hopes to see you again!");
+                    System.out.println("Bye Princess! দ্দি(˵ •̀ ᴗ - ˵ ) ✧\nPookie hopes to see you again!");
                     System.out.println("____________________________________________________________");
                     break;
-                } else if (in.equals("list")) {
+                } else if (in.equalsIgnoreCase("list")) {
                     System.out.println("____________________________________________________________");
                     if (!tasks.isEmpty()) {
+                        System.out.println("Here are the tasks in your list:");
                         for (int i = 0; i < tasks.size(); i++) {
-                            System.out.println("Here are the tasks in your list:");
                             System.out.println(" ˖˚⊹ ꣑ৎ\u200E " + (i + 1) + ". " + tasks.get(i).toString() + " ˖˚⊹ ꣑ৎ\u200E ");
                         }
                     } else {
@@ -33,85 +38,34 @@ public class Pookie {
                         System.out.println("______________________________________________________________");
                     }
                 } else if (in.startsWith("mark ")) {
-                    int index = Integer.parseInt(in.substring(5)) - 1;
-                    if (index < 0 || index >= tasks.size()) {
-                        throw new PookieException("Princess, there is no such task on your list!");
-                    }
-                    tasks.get(index).markDone();
-                    System.out.println("______________________________________________________________");
-                    System.out.println("Nice! ꉂ (≧ヮ≦) I've marked this task as done:");
-                    System.out.println(" " + tasks.get(index).toString());
-                    System.out.println("______________________________________________________________");
+                    handleMarkUnmark(tasks, in, true);
                 } else if (in.startsWith("unmark ")) {
-                    int index = Integer.parseInt(in.substring(7)) - 1;
-                    if (index < 0 || index >= tasks.size()) {
-                        throw new PookieException("Princess, there is no such task on your list!");
-                    }
-                    tasks.get(index).markNotDone();
-                    System.out.println("______________________________________________________________");
-                    System.out.println("OK... (¬_¬\") I've marked this task as not done yet:");
-                    System.out.println(" " + tasks.get(index).toString());
-                    System.out.println("______________________________________________________________");
-                } else if (in.startsWith("todo")) {
-                    String taskDescription = in.substring(4).trim();
-                    if (taskDescription.isEmpty()) {
-                        throw new PookieException.EmptyDescriptionException("Princess, the description of a todo cannot be empty.");
-                    }
-                    ToDo todoTask = new ToDo(taskDescription);
-                    tasks.add(todoTask);
-                    System.out.println("____________________________________________________________");
-                    System.out.println("Your wish is my command! I've added this task:");
-                    System.out.println(" " + todoTask);
-                    System.out.println("Now you have " + tasks.size() + " tasks in the list.");
-                    System.out.println("____________________________________________________________");
-                } else if (in.startsWith("deadline")) {
-                    String[] parts = in.substring(8).split(" /by ");
-                    if (parts[0].trim().isEmpty()) {
-                        throw new PookieException.EmptyDescriptionException("Princess, the description of a deadline cannot be empty.");
-                    }
-                    if (parts.length < 2 || parts[1].trim().isEmpty()) {
-                        throw new PookieException.MissingKeywordException("Princess, the deadline must have a /by date.");
-                    }
-                    String deadlineDescription = parts[0].trim();
-                    String byDate = parts[1].trim();
-                    Deadline deadlineTask = new Deadline(deadlineDescription, byDate);
-                    tasks.add(deadlineTask);
-                    System.out.println("______________________________________________________________");
-                    System.out.println("Gotcha! I've added this task:");
-                    System.out.println(" " + deadlineTask);
-                    System.out.println("Now you have " + tasks.size() + " tasks in the list");
-                    System.out.println("______________________________________________________________");
-                } else if (in.startsWith("event")) {
-                    String[] parts = in.substring(5).split(" /from | /to ");
-                    if (parts[0].trim().isEmpty()) {
-                        throw new PookieException.EmptyDescriptionException("Princess, the description of an event cannot be empty.");
-                    }
-                    if (parts.length < 3 || parts[1].trim().isEmpty() || parts[2].trim().isEmpty()) {
-                        throw new PookieException.MissingKeywordException("Princess, an event must have /from and /to times.");
-                    }
-                    String eventDescription = parts[0].trim();
-                    String fromDate = parts[1].trim();
-                    String toDate = parts[2].trim();
-                    Event eventTask = new Event(eventDescription, fromDate, toDate);
-                    tasks.add(eventTask);
-                    System.out.println("____________________________________________________________");
-                    System.out.println("Alrighty! I've added this task:");
-                    System.out.println(" " + eventTask);
-                    System.out.println("Now you have " + tasks.size() + " tasks in the list.");
-                    System.out.println("____________________________________________________________");
+                    handleMarkUnmark(tasks, in, false);
                 } else if (in.startsWith("delete")) {
-                    int index = Integer.parseInt(in.substring(6).trim()) - 1;
-                    if (index < 0 || index >= tasks.size()) {
-                        throw new PookieException.InvalidTaskIndexException("Princess, there is no such task to delete!");
-                    }
-                    Task removedTask = tasks.remove(index);
-                    System.out.println("____________________________________________________________");
-                    System.out.println("Okies! I've removed this task:");
-                    System.out.println(" " + removedTask.toString());
-                    System.out.println("Now you have " + tasks.size() + " tasks in the list.");
-                    System.out.println("____________________________________________________________");
+                    handleDelete(tasks, in);
                 } else {
-                    throw new PookieException("Sowwieeee (╥﹏╥) Pookie doesn't understand...");
+                    String[] parts = in.split(" ", 2);
+                    TaskType type;
+
+                    try {
+                        type = TaskType.valueOf(parts[0].toUpperCase());
+                    } catch (IllegalArgumentException e) {
+                        throw new PookieException("Sowwieeee (╥﹏╥) Pookie doesn't understand...");
+                    }
+
+                    switch (type) {
+                        case TODO:
+                            handleTodo(tasks, parts.length > 1 ? parts[1].trim() : "");
+                            break;
+                        case DEADLINE:
+                            handleDeadline(tasks, parts.length > 1 ? parts[1].trim() : "");
+                            break;
+                        case EVENT:
+                            handleEvent(tasks, parts.length > 1 ? parts[1].trim() : "");
+                            break;
+                        default:
+                            throw new PookieException("Sowwieeee (╥﹏╥) Pookie doesn't understand...");
+                    }
                 }
             } catch (PookieException e) {
                 System.out.println("____________________________________________________________");
@@ -124,5 +78,83 @@ public class Pookie {
             }
         }
         sc.close();
+    }
+
+    private static void handleMarkUnmark(ArrayList<Task> tasks, String in, boolean isMark) throws PookieException {
+        int index = Integer.parseInt(in.substring(isMark ? 5 : 7).trim()) - 1;
+        if (index < 0 || index >= tasks.size()) {
+            throw new PookieException("Princess, there is no such task on your list!");
+        }
+        if (isMark) {
+            tasks.get(index).markDone();
+            System.out.println("______________________________________________________________");
+            System.out.println("Nice! ꉂ (≧ヮ≦) I've marked this task as done:");
+        } else {
+            tasks.get(index).markNotDone();
+            System.out.println("______________________________________________________________");
+            System.out.println("OK... (¬_¬\") I've marked this task as not done yet:");
+        }
+        System.out.println(" " + tasks.get(index).toString());
+        System.out.println("______________________________________________________________");
+    }
+
+    private static void handleTodo(ArrayList<Task> tasks, String description) throws PookieException {
+        if (description.isEmpty()) {
+            throw new PookieException.EmptyDescriptionException("Princess, the description of a todo cannot be empty.");
+        }
+        ToDo todoTask = new ToDo(description);
+        tasks.add(todoTask);
+        System.out.println("____________________________________________________________");
+        System.out.println("Your wish is my command! I've added this task:");
+        System.out.println(" " + todoTask);
+        System.out.println("Now you have " + tasks.size() + " tasks in the list.");
+        System.out.println("____________________________________________________________");
+    }
+
+    private static void handleDeadline(ArrayList<Task> tasks, String details) throws PookieException {
+        String[] parts = details.split(" /by ");
+        if (parts[0].trim().isEmpty()) {
+            throw new PookieException.EmptyDescriptionException("Princess, the description of a deadline cannot be empty.");
+        }
+        if (parts.length < 2 || parts[1].trim().isEmpty()) {
+            throw new PookieException.MissingKeywordException("Princess, the deadline must have a /by date.");
+        }
+        Deadline deadlineTask = new Deadline(parts[0].trim(), parts[1].trim());
+        tasks.add(deadlineTask);
+        System.out.println("______________________________________________________________");
+        System.out.println("Gotcha! I've added this task:");
+        System.out.println(" " + deadlineTask);
+        System.out.println("Now you have " + tasks.size() + " tasks in the list.");
+        System.out.println("______________________________________________________________");
+    }
+
+    private static void handleEvent(ArrayList<Task> tasks, String details) throws PookieException {
+        String[] parts = details.split(" /from | /to ");
+        if (parts[0].trim().isEmpty()) {
+            throw new PookieException.EmptyDescriptionException("Princess, the description of an event cannot be empty.");
+        }
+        if (parts.length < 3 || parts[1].trim().isEmpty() || parts[2].trim().isEmpty()) {
+            throw new PookieException.MissingKeywordException("Princess, an event must have /from and /to times.");
+        }
+        Event eventTask = new Event(parts[0].trim(), parts[1].trim(), parts[2].trim());
+        tasks.add(eventTask);
+        System.out.println("____________________________________________________________");
+        System.out.println("Alrighty! I've added this task:");
+        System.out.println(" " + eventTask);
+        System.out.println("Now you have " + tasks.size() + " tasks in the list.");
+        System.out.println("____________________________________________________________");
+    }
+
+    private static void handleDelete(ArrayList<Task> tasks, String in) throws PookieException {
+        int index = Integer.parseInt(in.substring(6).trim()) - 1;
+        if (index < 0 || index >= tasks.size()) {
+            throw new PookieException("Princess, there is no such task to delete!");
+        }
+        Task removedTask = tasks.remove(index);
+        System.out.println("____________________________________________________________");
+        System.out.println("Okies! I've removed this task:");
+        System.out.println(" " + removedTask.toString());
+        System.out.println("Now you have " + tasks.size() + " tasks in the list.");
+        System.out.println("____________________________________________________________");
     }
 }
